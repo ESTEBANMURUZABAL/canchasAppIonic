550460288ac676593dfff941f30c6169af6a60e8
matias dynia <matias.dynia@wnco.com>
2016-10-12 16:04:09 -0500

    stuff

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -28,7 +28,6 @@ export class PopoverPage {
   templateUrl: 'about.html'
 })
 export class AboutPage {
-  conferenceDate = '2047-05-17';
 
   constructor(public popoverCtrl: PopoverController) { }
 
